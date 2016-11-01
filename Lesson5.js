4b65c957dc3086563ed89b5d91334fcd1e6049b2
Ria Stroes <riastroes@gmail.com>
2016-11-01 08:56:09 +0100

    Lessons 1-6
    update

--- Lesson5.js
+++ Lesson5.js
@@ -1,5 +1,5 @@
-/**
- * Created by Ria Stroes on 17-10-2016.
+/*
+ * Created by Ria Stroes on 25-10-2016.
  */
  "use strict";
  var s, nr;
@@ -9,27 +9,23 @@
  }
 
 function setup(){
-  nr = 4;
+  nr = 5;
 
   this.layout = new Layout(11,50,2);
-  this.layout.addSection(1,1,5,1,1);
-  this.layout.addSection(1,3,5,4,5);
-  this.layout.addSection(7,1,2,3,3);
-  this.layout.addSection(7,5,2,2,3);
-  this.layout.addSection(1,8,8,1,1);
-  this.layout.addSection(1,9,8,4,5);
-  this.layout.addSection(1,14,8,1,1);
-  this.layout.addSection(0,15,1,4,4); //lnr
-  this.layout.addSection(1,15,8,4,6);
-  this.layout.addSection(1,15,4,4,6);
-  this.layout.addSection(5,15,4,4,6);
-  this.layout.addSection(5,19,4,1,2);
-  this.layout.addSection(1,20,8,1,2);
-  this.layout.addSection(1,21,8,14,9);
-  this.layout.addSection(1,29,8,1,2);
-  this.layout.addSection(1,30,8,1,2);
+  this.layout.addSection(1,1,5,1,1); //0.title
+  this.layout.addSection(0,2,6,4,4); //1.img
+  this.layout.addSection(7,1,2,3,3); //2.bg nr
+  this.layout.addSection(7,5,2,2,3); //3.contentlist
+  this.layout.addSection(1,5,5,6,6); //4.intro
+  this.layout.addSection(1,11,8,1,1);
+  this.layout.addSection(0,13,1,4,4); //6.lnr
+  this.layout.addSection(1,12,8,5,4);// .how
 
+  this.layout.addSection(1,17,8,1,2);
+  this.layout.addSection(1,18,8,15,15);
 
+  this.layout.addSection(1,27,8,1,2);
+  this.layout.addSection(1,28,8,1,2);
 
   s = this.layout.sections[0];
   var bgTitle = createDiv("");
@@ -42,11 +38,16 @@ function setup(){
   title.size(s.width, s.height);
   title.class("lesson");
 
-
-  var subject = createDiv("Line");
+  var subject = createDiv("Variables");
   subject.position(s.x, s.y);
   subject.size(s.width, s.height);
   subject.class("subject");
+
+  s = this.layout.sections[1];
+  var imgbox = createDiv("");
+  imgbox.position(s.x, s.y);
+  imgbox.child("img01");
+  imgbox.class("bgimg");
 
   s = this.layout.sections[2];
   var bgDiv = createDiv("");
@@ -59,7 +60,20 @@ function setup(){
   lesnr.size(s.width, s.height);
   lesnr.class("nr");
 
-  s = this.layout.sections[1];
+  s = this.layout.sections[3];
+  var bgContent2 = createDiv("");
+  bgContent2.position(s.x, s.y);
+  bgContent2.size(s.width-40, s.height);
+  bgContent2.class("bgcontent");
+
+  var content2 = createDiv("");
+  bgContent2.child(content2);
+    content2.size(s.width-20, s.height);
+    content2.class("content");
+    content2.child(document.getElementById("content2"));
+
+
+  s = this.layout.sections[4];
   var bgContent1 = createDiv("");
   bgContent1.position(s.x, s.y);
   bgContent1.size(s.width-40, s.height);
@@ -76,26 +90,19 @@ function setup(){
 
 
 
-  s = this.layout.sections[3];
-  var bgContent2 = createDiv("");
-  bgContent2.position(s.x, s.y);
-  bgContent2.size(s.width-20, s.height);
-  bgContent2.class("bgcontent");
 
-  var content2 = createDiv("");
-  bgContent2.child(content2);
-    content2.size(s.width-20, s.height);
-    content2.class("content");
-    content2.child(document.getElementById("content2"));
-
-
-  s = this.layout.sections[4];
+  s = this.layout.sections[5];
   var howtitle = createDiv("HOW");
   howtitle.position(s.x, s.y);
   howtitle.size(s.width, s.height);
   howtitle.class("title");
+  s = this.layout.sections[6];
+    var bglnr = createDiv(nr);
+    bglnr.position(s.x, s.y);
+    bglnr.size(s.width, s.height);
+    bglnr.class("lnr");
 
-  s = this.layout.sections[5];
+  s = this.layout.sections[7];
   var how = createDiv("");
   how.position(s.x, s.y);
   how.size(s.width-40, s.height);
@@ -103,54 +110,52 @@ function setup(){
   how.child(document.getElementById("how1"));
   how.child(document.getElementById("how2"));
   how.child(document.getElementById("how3"));
-  how.child(document.getElementById("how4"));
-  how.child(document.getElementById("how5"));
 
-  s = this.layout.sections[6];
-  var canvastitle = createDiv("CANVAS");
-  canvastitle.position(s.x, s.y);
-  canvastitle.size(s.width, s.height);
-  canvastitle.class("canvastitle");
+  // s = this.layout.sections[7];
+  // var canvastitle = createDiv("CANVAS");
+  // canvastitle.position(s.x, s.y);
+  // canvastitle.size(s.width, s.height);
+  // canvastitle.class("canvastitle");
+  //
+  //
+  // s = this.layout.sections[9];
+  // var bgcanvas = createDiv("");
+  // bgcanvas.position(s.x, s.y);
+  // bgcanvas.size(s.width, s.height);
+  // bgcanvas.class("bgcanvas");
+  //
+  // s = this.layout.sections[10];
+  // var bgcode = createDiv("");
+  // bgcode.position(s.x, s.y);
+  // bgcode.size(s.width, s.height);
+  // bgcode.class("bgcode");
+  // bgcode.child(document.getElementById("code1"));
 
-  s = this.layout.sections[7];
-  var bglnr = createDiv(nr);
-  bglnr.position(s.x, s.y);
-  bglnr.size(s.width, s.height);
-  bglnr.class("lnr");
+
+  // s = this.layout.sections[11];
+  var canvas = createCanvas(0, 0);
+  // canvas.position(s.x, s.y);
+  // background(100);
+  // color1 = color(0);                //they need a value,
+  // color2 = color(180);              //before you can use them.
+  // color3 = color(0,0,200);
+  //
+  // frameRate(5);
+
+  // s = this.layout.sections[12];
+  // var link = createDiv("");
+  // link.position(s.x, s.y);
+  // link.size(s.width-40, s.height/2);
+  // link.class("link");
+  // link.child(document.getElementById("link1"));
 
   s = this.layout.sections[8];
-  var bgcanvas = createDiv("");
-  bgcanvas.position(s.x, s.y);
-  bgcanvas.size(s.width, s.height);
-  bgcanvas.class("bgcanvas");
-
-  s = this.layout.sections[9];
-  var bgcode = createDiv("");
-  bgcode.position(s.x, s.y);
-  bgcode.size(s.width, s.height);
-  bgcode.class("bgcode");
-  bgcode.child(document.getElementById("code1"));
-
-
-  s = this.layout.sections[10];
-  var canvas = createCanvas(s.width, s.height);
-  canvas.position(s.x, s.y);
-  background(255);
-
-  s = this.layout.sections[11];
-  var link = createDiv("");
-  link.position(s.x, s.y);
-  link.size(s.width-20, s.height/2);
-  link.class("link");
-  link.child(document.getElementById("link1"));
-
-  s = this.layout.sections[12];
   var asstitle = createDiv("ASSIGNMENTS");
   asstitle.position(s.x, s.y+30);
   asstitle.size(s.width, s.height/2);
   asstitle.class("title2");
 
-  s = this.layout.sections[13];
+  s = this.layout.sections[9];
   var ass = createDiv("");
   ass.position(s.x, s.y);
   ass.size(s.width-40, s.height/2);
@@ -159,44 +164,20 @@ function setup(){
   ass.child(document.getElementById("assinfo1"));
   ass.child(document.getElementById("assignment2"));
   ass.child(document.getElementById("assinfo2"));
+  ass.child(document.getElementById("assignment3"));
   ass.child(document.getElementById("assinfo3"));
 
-  s = this.layout.sections[14];
+  s = this.layout.sections[10];
   var question = createDiv("");
   question.position(s.x, s.y);
   question.size(s.width-20, s.height/2);
   question.class("info");
   question.child(document.getElementById("question"));
 
-  s = this.layout.sections[15];
+  s = this.layout.sections[11];
   var info = createDiv("");
   info.position(s.x, s.y);
   info.size(s.width-20, s.height/2);
   info.class("info");
   info.child(document.getElementById("info1"));
-
-
-}
-
-function draw(){
-
-    stroke(240);
-    strokeWeight(30);
-    line(85, 130, 200,155);
-
-    stroke(160);
-    strokeWeight(20);
-    line(50, 230, 200,-10);
-
-    stroke(80);
-    strokeWeight(10);
-    line(40, 250, 240,160);
-
-    stroke(0);
-    strokeWeight(1);
-    line(5, 290, 180,200);
-    line(180,200, 100,20);
-    line(100,20,5,290);
-
-
 }
--- Lesson6.js
+++ Lesson6.js
@@ -1,5 +1,5 @@
 /**
- * Created by Ria Stroes on 17-10-2016.
+ * Created by Ria Stroes on 25-10-2016.
  */
  "use strict";
  var s, nr;
@@ -9,27 +9,27 @@
  }
 
 function setup(){
-  nr = 4;
+  nr = 6;
 
   this.layout = new Layout(11,50,2);
   this.layout.addSection(1,1,5,1,1);
-  this.layout.addSection(1,3,5,4,5);
+  this.layout.addSection(1,3,5,4,4);
   this.layout.addSection(7,1,2,3,3);
   this.layout.addSection(7,5,2,2,3);
-  this.layout.addSection(1,8,8,1,1);
-  this.layout.addSection(1,9,8,4,5);
-  this.layout.addSection(1,14,8,1,1);
-  this.layout.addSection(0,15,1,4,4); //lnr
-  this.layout.addSection(1,15,8,4,6);
-  this.layout.addSection(1,15,4,4,6);
-  this.layout.addSection(5,15,4,4,6);
-  this.layout.addSection(5,19,4,1,2);
-  this.layout.addSection(1,20,8,1,2);
-  this.layout.addSection(1,21,8,14,9);
-  this.layout.addSection(1,29,8,1,2);
-  this.layout.addSection(1,30,8,1,2);
+  this.layout.addSection(1,8,8,3,3); //img
+  this.layout.addSection(1,11,8,1,1);
+  this.layout.addSection(1,12,8,7,7);
+  this.layout.addSection(1,20,8,3,1); //title canvas
+  this.layout.addSection(0,21,1,4,4); //lnr
+  this.layout.addSection(1,21,4,4,4);
+  this.layout.addSection(1,21,4,4,4);
+  this.layout.addSection(5,21,4,4,4);
+  this.layout.addSection(5,25,4,1,2);
+  this.layout.addSection(1,26,8,1,2);
+  this.layout.addSection(1,27,8,6,9);
 
-
+  this.layout.addSection(1,31,8,1,2);
+  this.layout.addSection(1,32,8,1,2);
 
   s = this.layout.sections[0];
   var bgTitle = createDiv("");
@@ -43,7 +43,7 @@ function setup(){
   title.class("lesson");
 
 
-  var subject = createDiv("Line");
+  var subject = createDiv("COLOR");
   subject.position(s.x, s.y);
   subject.size(s.width, s.height);
   subject.class("subject");
@@ -75,7 +75,6 @@ function setup(){
   content1.child(document.getElementById("intro3"));
 
 
-
   s = this.layout.sections[3];
   var bgContent2 = createDiv("");
   bgContent2.position(s.x, s.y);
@@ -88,14 +87,19 @@ function setup(){
     content2.class("content");
     content2.child(document.getElementById("content2"));
 
-
   s = this.layout.sections[4];
+  var imgcolor = createImg("resources/rgb.png");
+  imgcolor.position(s.x, s.y);
+  imgcolor.size(s.height, s.height);
+  imgcolor.class("imgcenter");
+
+  s = this.layout.sections[5];
   var howtitle = createDiv("HOW");
   howtitle.position(s.x, s.y);
   howtitle.size(s.width, s.height);
   howtitle.class("title");
 
-  s = this.layout.sections[5];
+  s = this.layout.sections[6];
   var how = createDiv("");
   how.position(s.x, s.y);
   how.size(s.width-40, s.height);
@@ -106,25 +110,25 @@ function setup(){
   how.child(document.getElementById("how4"));
   how.child(document.getElementById("how5"));
 
-  s = this.layout.sections[6];
+  s = this.layout.sections[7];
   var canvastitle = createDiv("CANVAS");
   canvastitle.position(s.x, s.y);
   canvastitle.size(s.width, s.height);
   canvastitle.class("canvastitle");
 
-  s = this.layout.sections[7];
+  s = this.layout.sections[8];
   var bglnr = createDiv(nr);
   bglnr.position(s.x, s.y);
   bglnr.size(s.width, s.height);
   bglnr.class("lnr");
 
-  s = this.layout.sections[8];
+  s = this.layout.sections[9];
   var bgcanvas = createDiv("");
   bgcanvas.position(s.x, s.y);
   bgcanvas.size(s.width, s.height);
   bgcanvas.class("bgcanvas");
 
-  s = this.layout.sections[9];
+  s = this.layout.sections[10];
   var bgcode = createDiv("");
   bgcode.position(s.x, s.y);
   bgcode.size(s.width, s.height);
@@ -132,25 +136,30 @@ function setup(){
   bgcode.child(document.getElementById("code1"));
 
 
-  s = this.layout.sections[10];
-  var canvas = createCanvas(s.width, s.height);
-  canvas.position(s.x, s.y);
-  background(255);
-
   s = this.layout.sections[11];
+  var canvas = createCanvas(s.width-20, s.height);
+  canvas.position(s.x, s.y);
+  background(100);
+  color1 = color(0);                //they need a value,
+  color2 = color(180);              //before you can use them.
+  color3 = color(0,0,200);
+
+  frameRate(5);
+
+  s = this.layout.sections[12];
   var link = createDiv("");
   link.position(s.x, s.y);
-  link.size(s.width-20, s.height/2);
+  link.size(s.width-40, s.height/2);
   link.class("link");
   link.child(document.getElementById("link1"));
 
-  s = this.layout.sections[12];
+  s = this.layout.sections[13];
   var asstitle = createDiv("ASSIGNMENTS");
   asstitle.position(s.x, s.y+30);
   asstitle.size(s.width, s.height/2);
   asstitle.class("title2");
 
-  s = this.layout.sections[13];
+  s = this.layout.sections[14];
   var ass = createDiv("");
   ass.position(s.x, s.y);
   ass.size(s.width-40, s.height/2);
@@ -159,17 +168,16 @@ function setup(){
   ass.child(document.getElementById("assinfo1"));
   ass.child(document.getElementById("assignment2"));
   ass.child(document.getElementById("assinfo2"));
-  ass.child(document.getElementById("assignment3"));
   ass.child(document.getElementById("assinfo3"));
 
-  s = this.layout.sections[14];
+  s = this.layout.sections[15];
   var question = createDiv("");
   question.position(s.x, s.y);
   question.size(s.width-20, s.height/2);
   question.class("info");
   question.child(document.getElementById("question"));
 
-  s = this.layout.sections[15];
+  s = this.layout.sections[16];
   var info = createDiv("");
   info.position(s.x, s.y);
   info.size(s.width-20, s.height/2);
@@ -178,26 +186,17 @@ function setup(){
 
 
 }
-
+var color1, color2, color3;
 function draw(){
 
-    stroke(240);
-    strokeWeight(30);
-    line(85, 130, 200,155);
+    stroke(color3);
+    strokeWeight(3);
+    fill(color2)
+    rect(random(width),random(height),random(50,100),random(10,50));
 
-    stroke(160);
-    strokeWeight(20);
-    line(50, 230, 200,-10);
-
-    stroke(80);
-    strokeWeight(10);
-    line(40, 250, 240,160);
-
-    stroke(0);
-    strokeWeight(1);
-    line(5, 290, 180,200);
-    line(180,200, 100,20);
-    line(100,20,5,290);
-
+    stroke(color3);
+    strokeWeight(5);
+    fill(color1)
+    rect(random(width),random(height),random(10,50),random(50,100));
 
 }
